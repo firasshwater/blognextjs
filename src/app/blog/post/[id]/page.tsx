123e02160215fb512118f7fa6b235d84e072f2bf
Firas Faris Shwater <firas25@hotmail.com>
2024-09-15 17:20:34 -0400

    added any type to fix build issue

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -2,10 +2,11 @@ import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
 export default function Page({ params }: { params: { id: string } }) {
-  const post = posts.find((post) => post.id === params.id);
+  const post1 = posts.find((post01) => post01.id === params.id);
   return (
     <>
       <h1>Post</h1>
-      <Post {...post} />
-    </>)
+      <Post {...post1} />
+    </>
+  );
 }
